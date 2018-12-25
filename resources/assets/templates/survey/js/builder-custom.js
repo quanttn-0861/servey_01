d29a1497a3994580fa0c8eba45d5e1bcc9a553ef
phanvantan73 <malong188@gmail.com>
2018-12-25 10:48:44 +0700

    fix bug 204812

--- resources/assets/templates/survey/js/builder-custom.js
+++ resources/assets/templates/survey/js/builder-custom.js
@@ -2711,10 +2711,12 @@ jQuery(document).ready(function () {
         $('.input-upload-image').trigger('click');
 
         $(document).on('change', '.input-upload-image', function () {
-            var formData = new FormData();
-            var url = $(this).data('url');
-            formData.append('image', this.files[0]);
-            uploadImage(formData, url);
+            if ($(this).val().length) {
+                var formData = new FormData();
+                var url = $(this).data('url');
+                formData.append('image', this.files[0]);
+                uploadImage(formData, url);
+            }
         });
     });
 
