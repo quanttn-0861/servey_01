14068ee20f028d35f46827816edba2fc140eb69f
phanvantan73 <malong188@gmail.com>
2019-02-22 14:48:38 +0700

    fix show section

--- resources/views/clients/survey/result/content_result.blade.php
+++ resources/views/clients/survey/result/content_result.blade.php
@@ -90,10 +90,10 @@
                         <li class="p-0">
                             <div class="form-header">
                                 <div class="section-badge section-option-menu">
-                                <span class="number-of-section">@lang('lang.section')
-                                    <span class="section-index">{{ $loop->iteration }}</span> /
-                                    <span class="total-section"></span>{{ count($resultsSurveys) }}
-                                </span>
+                                    <span class="number-of-section">@lang('lang.section')
+                                        <span class="section-index">{{ $loop->iteration }}</span> /
+                                        <span class="total-section"></span>{{ count($resultsSurveys) }}
+                                    </span>
                                     <div class="right-header-section">
                                         <a href="#" class="zoom-in-btn zoom-btn zoom-btn-result">
                                             <span class="zoom-icon"></span>
@@ -197,6 +197,10 @@
                         <li class="p-0">
                             <div class="form-header">
                                 <div class="section-badge section-option-menu">
+                                    <span class="number-of-redirect">@lang('lang.redirect')
+                                        <span class="section-index">{{ $loop->iteration }}</span> /
+                                        <span class="total-section"></span>{{ count($resultsSurveys) }}
+                                    </span>
                                     <div class="right-header-section">
                                         <a href="#" class="zoom-in-btn zoom-btn zoom-btn-result">
                                             <span class="zoom-icon"></span>
@@ -252,110 +256,112 @@
                 @endforeach
 
                 @foreach ($publicResults as $resultsSurvey)
-                    <ul class="clearfix form-wrapper ul-result wrapper-section-result">
-                        <li class="p-0">
-                            <div class="form-header">
-                                <div class="section-badge section-option-menu">
-                                    <span class="number-of-section">@lang('lang.section')
-                                        <span class="section-index">{{ $loop->iteration }}</span> /
-                                        <span class="total-section"></span>{{ count($publicResults) }}
-                                    </span>
-                                    <div class="right-header-section">
-                                        <a href="#" class="zoom-in-btn zoom-btn zoom-btn-result">
-                                            <span class="zoom-icon"></span>
-                                        </a>
-                                    </div>
-                                </div>
-                                <hr/>
-                                <h3 class="title-section" data-placement="bottom" data-toggle="tooltip"
-                                    title="{{ $resultsSurvey['section']->showTitleTooltip() }}">
-                                    {!! nl2br(e($resultsSurvey['section']->limit_title)) !!}
-                                </h3>
-                                <span class="description-section-result">
-                                    {!! nl2br(e($resultsSurvey['section']->custom_description)) !!}
-                                </span>
-                            </div>
-                        </li>
-                    </ul>
-
-                    <ul class="clearfix form-wrapper ul-result content-section-result">
-                        @php
-                            $indexQuestion = 0;
-                        @endphp
-
-                        @foreach ($resultsSurvey['question_result'] as $result)
-                            <li class="li-question-review form-line li-question-result">
-                                @if ($result['question_type'] == config('settings.question_type.title'))
-                                    <div class="title-question-preview">
-                                        <span>{!! nl2br(e($result['question']->title)) !!}</span>
-                                    </div>
-                                    <div class="form-group form-group-description-section">
-                                        <span class="description-section">
-                                            {!! nl2br(e($result['question']->description)) !!}
+                    @if ($resultsSurvey['section']->countNoneRedirectQuestions())
+                        <ul class="clearfix form-wrapper ul-result wrapper-section-result">
+                            <li class="p-0">
+                                <div class="form-header">
+                                    <div class="section-badge section-option-menu">
+                                        <span class="number-of-section">@lang('lang.section')
+                                            <span class="section-index">{{ $loop->iteration }}</span> /
+                                            <span class="total-section"></span>{{ count($publicResults) }}
                                         </span>
+                                        <div class="right-header-section">
+                                            <a href="#" class="zoom-in-btn zoom-btn zoom-btn-result">
+                                                <span class="zoom-icon"></span>
+                                            </a>
+                                        </div>
                                     </div>
-                                @else
-                                    @if ($result['question']->type != config('settings.question_type.redirect'))
-                                        <span class="index-question">{{ ++ $indexQuestion }}</span>
-                                        <h4 class="title-question">
-                                            {!! nl2br(e($result['question']->title)) !!}
-                                            @if ($result['question']->required)
-                                                <span class="notice-required-question"> *</span>
-                                            @endif
-                                        </h4>
-                                        @if ($result['question']->media->count())
-                                            <div class="img-preview-question-survey">
-                                                {!! Html::image($result['question']->url_media) !!}
-                                            </div>
-                                        @endif
+                                    <hr/>
+                                    <h3 class="title-section" data-placement="bottom" data-toggle="tooltip"
+                                        title="{{ $resultsSurvey['section']->showTitleTooltip() }}">
+                                        {!! nl2br(e($resultsSurvey['section']->limit_title)) !!}
+                                    </h3>
+                                    <span class="description-section-result">
+                                        {!! nl2br(e($resultsSurvey['section']->custom_description)) !!}
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul class="clearfix form-wrapper ul-result content-section-result">
+                            @php
+                                $indexQuestion = 0;
+                            @endphp
+
+                            @foreach ($resultsSurvey['question_result'] as $result)
+                                <li class="li-question-review form-line li-question-result">
+                                    @if ($result['question_type'] == config('settings.question_type.title'))
+                                        <div class="title-question-preview">
+                                            <span>{!! nl2br(e($result['question']->title)) !!}</span>
+                                        </div>
                                         <div class="form-group form-group-description-section">
-                                            <span class="number-result-answer">
-                                                {{ $result['count_answer'] }} @lang('result.number_answer')
+                                            <span class="description-section">
+                                                {!! nl2br(e($result['question']->description)) !!}
                                             </span>
                                         </div>
-                                        @if ($result['answers'])
-                                            @if (in_array($result['question']->type, [
-                                                config('settings.question_type.short_answer'),
-                                                config('settings.question_type.long_answer'),
-                                                config('settings.question_type.date'),
-                                                config('settings.question_type.time'),
-                                            ]))
-                                                <div class="answer-result scroll-answer-result" id="style-scroll-3">
-                                                    @foreach ($result['answers'] as $answer)
-                                                        <p class="{{ ($loop->iteration % config('settings.checkEventOdd')) ?
-                                                            'item-answer-result-even' :
-                                                            'item-answer-result-odd' }}"
-                                                            data-placement="bottom" data-toggle="tooltip"
-                                                            title="{{ $answer['content'] }}">
-                                                            {{ str_limit($answer['content'], config('settings.limit_answer_content')) }}
-                                                            <span class="percent-answer">({{ $answer['percent'] }}%)</span>
-                                                        </p>
-                                                    @endforeach
+                                    @else
+                                        @if ($result['question']->type != config('settings.question_type.redirect'))
+                                            <span class="index-question">{{ ++ $indexQuestion }}</span>
+                                            <h4 class="title-question">
+                                                {!! nl2br(e($result['question']->title)) !!}
+                                                @if ($result['question']->required)
+                                                    <span class="notice-required-question"> *</span>
+                                                @endif
+                                            </h4>
+                                            @if ($result['question']->media->count())
+                                                <div class="img-preview-question-survey">
+                                                    {!! Html::image($result['question']->url_media) !!}
                                                 </div>
-                                            @elseif ($result['question_type'] == config('settings.question_type.multiple_choice'))
-                                                @if ($result['question']->answers->count())
-                                                    <div class="answer-result chart-result-answer multiple-choice-result"
-                                                        id="{{ $result['question']->id }}"
-                                                        data="{{ json_encode($result['answers']) }}"></div>
-                                                @endif
-                                            @elseif ($result['question_type'] == config('settings.question_type.checkboxes'))
-                                                @if ($result['question']->answers->count())
-                                                    <div class="answer-result chart-result-answer checkboxes-result"
-                                                        id="{{ $result['question']->id }}"
-                                                        data="{{ json_encode($result['answers']) }}"></div>
-                                                @endif
                                             @endif
-                                        @else
-                                            <span class="no-answer">@lang('result.there_is_no_result')</span>
+                                            <div class="form-group form-group-description-section">
+                                                <span class="number-result-answer">
+                                                    {{ $result['count_answer'] }} @lang('result.number_answer')
+                                                </span>
+                                            </div>
+                                            @if ($result['answers'])
+                                                @if (in_array($result['question']->type, [
+                                                    config('settings.question_type.short_answer'),
+                                                    config('settings.question_type.long_answer'),
+                                                    config('settings.question_type.date'),
+                                                    config('settings.question_type.time'),
+                                                ]))
+                                                    <div class="answer-result scroll-answer-result" id="style-scroll-3">
+                                                        @foreach ($result['answers'] as $answer)
+                                                            <p class="{{ ($loop->iteration % config('settings.checkEventOdd')) ?
+                                                                'item-answer-result-even' :
+                                                                'item-answer-result-odd' }}"
+                                                                data-placement="bottom" data-toggle="tooltip"
+                                                                title="{{ $answer['content'] }}">
+                                                                {{ str_limit($answer['content'], config('settings.limit_answer_content')) }}
+                                                                <span class="percent-answer">({{ $answer['percent'] }}%)</span>
+                                                            </p>
+                                                        @endforeach
+                                                    </div>
+                                                @elseif ($result['question_type'] == config('settings.question_type.multiple_choice'))
+                                                    @if ($result['question']->answers->count())
+                                                        <div class="answer-result chart-result-answer multiple-choice-result"
+                                                            id="{{ $result['question']->id }}"
+                                                            data="{{ json_encode($result['answers']) }}"></div>
+                                                    @endif
+                                                @elseif ($result['question_type'] == config('settings.question_type.checkboxes'))
+                                                    @if ($result['question']->answers->count())
+                                                        <div class="answer-result chart-result-answer checkboxes-result"
+                                                            id="{{ $result['question']->id }}"
+                                                            data="{{ json_encode($result['answers']) }}"></div>
+                                                    @endif
+                                                @endif
+                                            @else
+                                                <span class="no-answer">@lang('result.there_is_no_result')</span>
+                                            @endif
                                         @endif
                                     @endif
-                                @endif
-                            </li>
-                        @endforeach
-                        @if (!count($resultsSurvey['question_result']))
-                            <div class="nothing-to-collect">@lang('result.nothing_to_collect')</div>
-                        @endif
-                    </ul>
+                                </li>
+                            @endforeach
+                            @if (!count($resultsSurvey['question_result']))
+                                <div class="nothing-to-collect">@lang('result.nothing_to_collect')</div>
+                            @endif
+                        </ul>
+                    @endif
                 @endforeach
             @endif
         </div>
