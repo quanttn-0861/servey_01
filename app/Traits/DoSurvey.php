f756fe44896aa5dae5c35def964d4944eab3561c
phanvantan73 <malong188@gmail.com>
2019-07-01 15:43:47 +0700

    fix bug

--- app/Traits/DoSurvey.php
+++ app/Traits/DoSurvey.php
@@ -12,12 +12,14 @@ trait DoSurvey
         if ($survey->sections->where('update', config('settings.survey.section_update.updated'))->count() && Auth::user()) {
             $lastResults = $survey->results->where('user_id', Auth::user()->id)->groupBy('token')->last();
 
-            if ($lastResults) {
+            if ($lastResults->count()) {
                 $redirectIds = $survey->sections->where('redirect_id', '!=', 0)->sortBy('order')->pluck('redirect_id', 'id')->all();
 
                 foreach ($redirectIds as $key => $redirectId) {
+                    $questionRedirect = $this->answerRepository->where('id', $redirectId)->first()->question;
+                    $lastResult = $lastResults->where('question_id', $questionRedirect->id)->first();
 
-                    if (!$lastResults->where('answer_id', $redirectId)->first()) {
+                    if ($lastResult && $lastResult->answer_id != $redirectId) {
                         $sectionIds = array_diff($sectionIds, [$key]);
                     }
                 }
@@ -30,7 +32,6 @@ trait DoSurvey
             'section' => $section,
             'index_section' => count($sectionIds) == 1 ? config('settings.index_section.end') : config('settings.index_section.start'),
             'section_ids' => $sectionIds,
-
         ];
     }
 }
